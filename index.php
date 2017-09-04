2312c7943a56fd6baf3fdd17f96b6b14ce5aef6d
nathaniel <nathaniel@AOC>
2017-09-04 09:02:02 +0200

    suppression du travail des autres modification du chemin du localhost ajout des commentaires

--- index.php
+++ index.php
@@ -7,19 +7,12 @@ include 'includes/header.php'; //inclusion de l'header dans toute les pages.
     <?php
     include 'includes/nav_bar.php'; //inclusion de la la nav bar dans toute les pages.
     ?>
-</body>
-
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-        
-    </head>
-    <body>
     <table>
         <?php
         include 'includes/list_process.php';
         ?>
     </table>
-    </body>
+</body>
+
+
 </html>
