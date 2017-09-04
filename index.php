f815a582ab4f5706ef92c214fd4790993b0975fc
nathaniel <nathaniel@AOC>
2017-09-04 09:02:02 +0200

    suppression du travail des autres modification du chemin du localhost ajout des commentaires

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <body>
     <table>
         <?php
-        include 'includes/list_process.php';
+        include 'includes/list_process.php';//inclut le resultat de list process.php dans des balises <table>
         ?>
     </table>
     </body>
