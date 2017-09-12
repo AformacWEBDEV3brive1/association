05176228dc3f87eabf38865c72c1ff12f706c51f
Alwahahnu <30408238+Alwahahnu@users.noreply.github.com>
2017-09-12 09:58:27 +0200

    suppresion du session_destroy()

--- index.php
+++ index.php
@@ -7,9 +7,8 @@ include 'includes/nav_bar.php'; //inclusion de la la nav bar dans toute les page
 
 <body>
     <?php
-    session_start();
-    echo $_SESSION['mess_err'];
-    session_destroy();
+    
+    
     //on detruit la session aprés reception du message. 
        if ($_SESSION['log'] == null) {
         session_destroy();
