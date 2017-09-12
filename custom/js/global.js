301084440035a2abd901ef79dee3ea76dc09d908
ezaltar <ezaltar@ezaltar-HP-ProBook-450-G3>
2017-09-12 08:02:02 +0200

    début de la mise en forme de l'accueil

--- custom/js/global.js
+++ custom/js/global.js
@@ -94,7 +94,7 @@ function user_delete() {
 }
 
 
-//genere l'accueil.
+//genere l'accueil. Non utilisée, a virer.
 function generate_accueil() {
 
     $.ajax({url: '/association/includes/accueil_site.php',
--- index.php
+++ index.php
@@ -1,24 +1,39 @@
 <?php
-
 include 'includes/header.php'; //inclusion de l'header dans toute les pages.
+include 'includes/accueil_site.php';
 ?>
 
 
-<body onload="generate_accueil()">
+<!--<body onload="generate_accueil()">-->
+<body>
     <?php
     include 'includes/nav_bar.php'; //inclusion de la la nav bar dans toute les pages.
-     
-    ?>
-   <?php
-   
+
     session_start();
     echo $_SESSION['mess_err'];
     session_destroy();
-   //on detruit la session aprés reception du message.
-         
-          
-   ?>
-    <div id='accueil'></div>
+    //on detruit la session aprés reception du message.
+    ?>
+
+    <?php $res = affiche_accueil(); ?>
+
+    <div class="container">
+        <div id='accueil'>
+            
+            <h1 class="text-center"> <?php echo $res['titre']; ?> </h1>
+            
+            <img class="taille_photo" src="<?php echo $res['photo']; ?>" />
+            <div class="espace_message">
+            <h1 class="background_message"> <?php echo $res['message']; ?> </h1>
+            </div>
+            
+            <img class="taille_photo"src=" <?php echo $res['image'] ?>" />
+            
+            <a href=" <?php echo $res['reseaux'] ?> "> Facebook </a>
+            
+
+        </div>
+    </div>
 </body>
 
 
