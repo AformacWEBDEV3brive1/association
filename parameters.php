0b3724a05315c1027020ccbedbcc64272d87a2a0
christian <christian@christian-HP-ProBook-450-G3>
2017-09-18 10:02:44 +0200

    commit pour pull

--- parameters.php
+++ parameters.php
@@ -4,7 +4,7 @@ $linux_user= "ezaltar";
 
 $connexion_string =  "mysql:host=127.0.0.1;dbname=association;charset=utf8";
 $login = "root";
-$mdp = "ezaltar";
+$mdp = "123456789$";
 
 function openBDD()
 {
